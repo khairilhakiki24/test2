db1c214030308f3b0ac46a1633f6d4248192ee01
khairilhakiki24 <kikikrenz@gmail.com>
2026-03-28 17:36:18 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'kasirsync-cache-v4';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/icon-192x192.png',
+  './assets/icon-512x512.png',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://unpkg.com/lucide@latest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                if(event.request.method === 'GET' && event.request.url.startsWith('http')){
+                   cache.put(event.request, responseToCache);
+                }
+              });
+            return response;
+          }
+        );
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
